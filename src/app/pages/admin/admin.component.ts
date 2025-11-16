d1957a71ecfc26e6f5034784e78a99a33769d59e
ramisoul84 <eng.rami.suliman@gmail.com>
2025-11-16 21:54:26 +0300

    contact

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  isLoggedin: boolean = false
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit(): void {
+  }
 }
--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -2,18 +2,28 @@ import { Component, inject } from '@angular/core';
 import { GridService } from '../../services/grid.service';
 import { CommonModule } from '@angular/common';
 import { GoogleAnalyticsService } from 'ngx-google-analytics';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  msg: string = ""
   height: number = 0
+  isSubmitting: boolean = false;
+
+  contactForm: FormGroup;
 
   private gaService = inject(GoogleAnalyticsService);
-  constructor(private gridService: GridService) {
+  constructor(private gridService: GridService, private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(3)]]
+    });
   }
 
   ngOnInit(): void {
@@ -53,8 +63,15 @@ export class ContactComponent {
     this.onLinkClick(url, "email");
   }
 
-    sendMessage(msg: string): void {
+  sendMessage(): void {
+    this.isSubmitting = true
 
+    setTimeout(() => {
+      this.isSubmitting = false,
+        setTimeout(() => { this.msg=""}, 2000)
+      this.msg = "✅ Thank you! Your message has been sent successfully."
+      //  ❌ Sorry, there was an error sending your message. Please try again.
+    }, 3000)
   }
 
 }
